307193c9b4b9cc70711f759b532ca6312d269360
Thomas Broyer <t.broyer@ltgt.net>
2017-11-12 16:56:47 +0100

    Import com.google.gwt.user.History to org.gwtproject.user.history.History

---
+++ src/test/java/org/gwtproject/user/history/HistorySuite.java
@@ -0,0 +1,12 @@
+package org.gwtproject.user.history;
+
+import org.gwtproject.user.history.client.HistoryTest;
+import org.gwtproject.user.history.client.HistoryTestNoopTokenEncoder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ HistoryTest.class, HistoryTestNoopTokenEncoder.class })
+public class HistorySuite {
+}
