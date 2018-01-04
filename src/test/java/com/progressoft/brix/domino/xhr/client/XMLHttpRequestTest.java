a0aff7831bdf325e33e0f686868b35dd2a8a3d22
rjeeb <rafat.albarouki@gmail.com>
2018-01-05 00:43:05 +0200

    more tests

--- src/test/java/com/progressoft/brix/domino/xhr/client/XMLHttpRequestTest.java
+++ src/test/java/com/progressoft/brix/domino/xhr/client/XMLHttpRequestTest.java
@@ -10,6 +10,9 @@ public class XMLHttpRequestTest extends GWTTestCase {
 
     private static final Logger LOGGER = Logger.getLogger(XMLHttpRequestTest.class.getCanonicalName());
     private static final int UNSENT = 0;
+    private static final int OPENED = 1;
+    private static final int DONE = 4;
+    private XMLHttpRequest xmlHttpRequest;
 
     @Override
     public String getModuleName() {
@@ -32,51 +35,64 @@ public class XMLHttpRequestTest extends GWTTestCase {
         timer.schedule(100);
     }
 
-    public void testCreateXmlHttpRequest() {
-        XMLHttpRequest xmlHttpRequest = XMLHttpRequest.create();
-
-        assertNotNull(xmlHttpRequest);
-        assertEquals(UNSENT, xmlHttpRequest.getReadyState());
-    }
-
-    public void testOpenXmlHttpRequest() {
-        XMLHttpRequest xmlHttpRequest = XMLHttpRequest.create();
-        xmlHttpRequest.setOnReadyStateChange(xhr -> {
-            assertEquals(1, xhr.getReadyState());
-            finishTest();
-        });
-
-        runAsyncTask(() -> xmlHttpRequest.open("GET", "url"));
-    }
-
-    public void testOpenWithUserXmlHttpRequest() {
-        XMLHttpRequest xmlHttpRequest = XMLHttpRequest.create();
-        xmlHttpRequest.setOnReadyStateChange(xhr -> {
-            assertEquals(1, xhr.getReadyState());
-            finishTest();
-        });
-
-        runAsyncTask(() -> xmlHttpRequest.open("GET", "url", "user"));
-    }
-
-    public void testOpenWithUserAndPasswordXmlHttpRequest() {
-        XMLHttpRequest xmlHttpRequest = XMLHttpRequest.create();
-        xmlHttpRequest.setOnReadyStateChange(xhr -> {
-            assertEquals(1, xhr.getReadyState());
-            finishTest();
-        });
-
-        runAsyncTask(() -> xmlHttpRequest.open("GET", "url", "user", "password"));
-    }
-
-    public void testAbortXmlHttpRequest() {
-        XMLHttpRequest xmlHttpRequest = XMLHttpRequest.create();
-        xmlHttpRequest.open("GET", getTestPath());
-        xmlHttpRequest.send();
-    }
-
     private String getTestPath() {
         return GWT.getModuleBaseURL() + "testRequest";
     }
 
+    @Override
+    protected void gwtSetUp() {
+        xmlHttpRequest = XMLHttpRequest.create();
+    }
+
+    public void testCreateXmlHttpRequest_shouldNotBeNull() {
+        assertNotNull(xmlHttpRequest);
+        assertEquals(UNSENT, xmlHttpRequest.getReadyState());
+    }
+
+    public void testOpenXmlHttpRequest_shouldHaveReadyStateOfOPENED() {
+        xmlHttpRequest.setOnReadyStateChange(xhr -> {
+            assertEquals(OPENED, xhr.getReadyState());
+            finishTest();
+        });
+        runAsyncTask(() -> xmlHttpRequest.open("GET", "url"));
+    }
+
+    public void testOpenWithUserXmlHttpRequest_shouldHaveReadyStateOfOPENED() {
+        xmlHttpRequest.setOnReadyStateChange(xhr -> {
+            assertEquals(OPENED, xhr.getReadyState());
+            finishTest();
+        });
+        runAsyncTask(() -> xmlHttpRequest.open("GET", getTestPath(), "user"));
+    }
+
+    public void testOpenWithUserAndPasswordXmlHttpRequest() {
+        xmlHttpRequest.setOnReadyStateChange(xhr -> {
+            assertEquals(OPENED, xhr.getReadyState());
+            finishTest();
+        });
+        runAsyncTask(() -> xmlHttpRequest.open("GET", "url", "user", "password"));
+    }
+
+    public void testSetRequestHeader() {
+        xmlHttpRequest.setOnReadyStateChange(xhr -> {
+            if (xhr.getReadyState() == DONE) {
+                assertEquals("value", xhr.getResponseHeader("header"));
+                finishTest();
+            }
+        });
+        xmlHttpRequest.open("GET", getTestPath());
+        xmlHttpRequest.setRequestHeader("header", "value");
+        runAsyncTask(xmlHttpRequest::send);
+    }
+
+    public void testAllResponseHeaders() {
+        xmlHttpRequest.setOnReadyStateChange(xhr -> {
+            if (xhr.getReadyState() == DONE) {
+                LOGGER.info(xmlHttpRequest.getAllResponseHeaders());
+                finishTest();
+            }
+        });
+        xmlHttpRequest.open("GET", getTestPath());
+        runAsyncTask(xmlHttpRequest::send);
+    }
 }
--- src/test/java/com/progressoft/brix/domino/xhr/server/TestServlet.java
+++ src/test/java/com/progressoft/brix/domino/xhr/server/TestServlet.java
@@ -3,6 +3,8 @@ package com.progressoft.brix.domino.xhr.server;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.logging.Logger;
 
 public class TestServlet extends HttpServlet {
@@ -10,7 +12,12 @@ public class TestServlet extends HttpServlet {
     private static final Logger LOGGER = Logger.getLogger(TestServlet.class.getCanonicalName());
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        LOGGER.info("getttt");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            resp.setHeader(headerName, req.getHeader(headerName));
+        }
+        resp.setStatus(200);
     }
 }
