d4fd5b9fab96fbc3acf10370ee3cb9e639dd3b41
Thomas Broyer <t.broyer@ltgt.net>
2018-08-31 14:48:40 +0200

    Update google-java-format plugin and ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.time.Year
 plugins {
     `java-library`
     id("net.ltgt.errorprone-javacplugin") version "0.5"
-    id("com.github.sherter.google-java-format") version "0.6"
+    id("com.github.sherter.google-java-format") version "0.7.1"
     id("com.github.hierynomus.license") version "0.14.0"
     id("local.ktlint")
     id("local.maven-publish")
