c061278d73c89b62880e345cbfe4aeca909db0fd
kot <stas@jresearch.org>
2020-12-12 13:53:09 +0100

    Add demo application

---
+++ time/src/main/java/org/jresearch/threetenbp/gwt/time/client/TimeEntryPoint.java
@@ -0,0 +1,12 @@
+package org.jresearch.threetenbp.gwt.time.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class TimeEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Support.init();
+	}
+
+}
