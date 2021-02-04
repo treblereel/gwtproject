2be81b77f09b5e486189eae042811e50be98e8a9
kot <stas@jresearch.org>
2021-02-04 18:47:43 +0100

    Remove asynch loading, because it doulbe transfered data and complicate the logic

--- tzdb/src/main/java/org/jresearch/threetenbp/gwt/tzdb/client/loader/TzdbJsBundle.java
+++ tzdb/src/main/java/org/jresearch/threetenbp/gwt/tzdb/client/loader/TzdbJsBundle.java
@@ -3,7 +3,6 @@ package org.jresearch.threetenbp.gwt.tzdb.client.loader;
 import javax.annotation.Nonnull;
 
 import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.TextResource;
 
 public interface TzdbJsBundle extends ClientBundle {
@@ -11,10 +10,6 @@ public interface TzdbJsBundle extends ClientBundle {
 	@Nonnull
 	@Source("base64-binary.js")
 	public TextResource base64binary();
-
-	@Nonnull
-	@Source("TZDB.dat")
-	public DataResource tzdb();
 
 	@Nonnull
 	@Source("TZDB.txt")
