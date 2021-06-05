7bdc041cab400d5922757f97017a1a41d844c2c8
kot <stas@jresearch.org>
2021-06-05 18:35:32 +0200

    Use package relocation

--- time/src/main/java/org/jresearch/threetenbp/gwt/time/client/TimeEntryPoint.java
+++ time/src/main/java/org/jresearch/threetenbp/gwt/time/client/TimeEntryPoint.java
@@ -1,7 +1,5 @@
 package org.jresearch.threetenbp.gwt.time.client;
 
-import org.jresearch.threetenbp.gwt.emu.org.jresearch.threetenbp.gwt.time.client.Support;
-
 import com.google.gwt.core.client.EntryPoint;
 
 public class TimeEntryPoint implements EntryPoint {
