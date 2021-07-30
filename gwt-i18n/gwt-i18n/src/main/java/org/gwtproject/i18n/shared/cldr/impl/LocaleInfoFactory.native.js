b6d33d1622f11b4533910a4eed6f2a19d0edc5f8
Dmitrii Tikhomirov <chani.liet@gmail.com>
2021-07-30 00:31:14 -0400

    gwt i18n improved stability

--- gwt-i18n/gwt-i18n/src/main/java/org/gwtproject/i18n/shared/cldr/impl/LocaleInfoFactory.native.js
+++ gwt-i18n/gwt-i18n/src/main/java/org/gwtproject/i18n/shared/cldr/impl/LocaleInfoFactory.native.js
@@ -14,18 +14,17 @@
  * limitations under the License.
  */
 
-let org_gwtproject_i18n_shared_cldr_impl_LocaleInfoImpl = goog.forwardDeclare('org.gwtproject.i18n.shared.cldr.impl.LocaleInfoImpl$impl');
+let localeInfoImpl = goog.forwardDeclare('org.gwtproject.i18n.shared.cldr.impl.LocaleInfoImpl$impl');
 
 org_gwtproject_i18n_shared_cldr_impl_LocaleInfoFactory.get = function(locale) {
   LocaleInfoFactory.$clinit();
   if (LocaleInfoFactory.$static_holder__org_gwtproject_i18n_shared_cldr_impl_LocaleInfoFactory.has(locale)) {
    return LocaleInfoFactory.$static_holder__org_gwtproject_i18n_shared_cldr_impl_LocaleInfoFactory.get(locale);
   }
-  return org_gwtproject_i18n_shared_cldr_impl_LocaleInfoImpl.$create__();
+  localeInfoImpl = goog.module.get('org.gwtproject.i18n.shared.cldr.impl.LocaleInfoImpl$impl');
+  return localeInfoImpl.$create__();
 };
 
 org_gwtproject_i18n_shared_cldr_impl_LocaleInfoFactory.m_getLocale__ = function() {
-    console.log("m_getLocale__");
-
   return $Util.$getDefine("gwt.locale");
 }
